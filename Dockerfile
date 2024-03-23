87f23996c0cbab5e2c1142123067d281a0dd8bbe
GesundesWasser <samuelpernter@gmail.com>
2024-03-23 15:26:35 +0100

    fix the docker bug

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
 FROM alpine:latest
 
+COPY server.sh .
 RUN apk update \
     && apk add npm \
     && apk add curl \
     && apk add nodejs \
     && apk add bash \
     && mkdir /server
-
+COPY package.json /server
+RUN cd /server && npm install
 # Start script
 CMD ["bash", "./server.sh"]
 
 # Container setup
 EXPOSE 3000/tcp
 EXPOSE 3000/udp
-VOLUME /server
--- server.sh
+++ server.sh
@@ -3,13 +3,7 @@
 # Enter server directory
 cd server
 
-if [ ! -e "install.txt" ]; then
-    npm install
-    echo "install=true" > "install.txt
-    echo "Installing Server..."
-    curl -o server.js https://raw.githubusercontent.com/GesundesWasser/McAccount-Server/master/server.js
-else
+# Start nodejs server
 rm -rf server.js
-curl -o server.js https://raw.githubusercontent.com/GesundesWasser/McAccount-Server/master/server.js
+curl -so server.js https://raw.githubusercontent.com/GesundesWasser/McAccount-Server/master/server.js
 node server.js
-fi
